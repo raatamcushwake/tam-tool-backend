fd62b0badb9536544edf4228bf61f54e3b6d8582
raatamcushwake <raatam.cushwake@gmail.com>
2026-05-15 18:17:07 +0530

    Update firebase.py

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -1,12 +1,19 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
+import json
+import os
 
 _app = None
 
 def init_firebase():
     global _app
     if not firebase_admin._apps:
-        cred = credentials.Certificate("serviceAccountKey.json")
+        key_json = os.environ.get("FIREBASE_SERVICE_ACCOUNT_KEY")
+        if key_json:
+            key_dict = json.loads(key_json)
+            cred = credentials.Certificate(key_dict)
+        else:
+            cred = credentials.Certificate("serviceAccountKey.json")
         _app = firebase_admin.initialize_app(cred)
     return _app
 
