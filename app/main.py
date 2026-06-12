2f7f179a75eb0222d9d7c6cd34e012c93383a31b
raatamcushwake <raatam.cushwake@gmail.com>
2026-06-13 01:35:40 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -24,12 +24,19 @@ app = FastAPI(
 )
 
 # CORS Middleware
-allow_origins=[
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://tam-tool-frontend.onrender.com",
-    "https://tam-cushman-wakfield.onrender.com",
-],
+# CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://tam-tool-frontend.onrender.com",
+        "https://tam-cushman-wakfield.onrender.com",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Register Routes
