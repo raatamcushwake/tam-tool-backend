7868d2cbb2273ef5b19b66ee5188b8db9e66d39f
raatamcushwake <raatam.cushwake@gmail.com>
2026-06-13 01:29:21 +0530

    Update CORS middleware to include new origin
    Added new allowed origin for CORS middleware.

--- app/main.py
+++ app/main.py
@@ -24,13 +24,12 @@ app = FastAPI(
 )
 
 # CORS Middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://tam-tool-frontend.onrender.com"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://tam-tool-frontend.onrender.com",
+    "https://tam-cushman-wakfield.onrender.com",
+],
 
 
 # Register Routes
