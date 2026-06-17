8e62edf28865e34a9bcf4cd6b3843fae4c5cba6e
raatamcushwake <raatam.cushwake@gmail.com>
2026-06-17 12:55:24 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -29,7 +29,11 @@ app = FastAPI(
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://tam-cushman-wakfield.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
