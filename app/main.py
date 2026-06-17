ffdb5b1e98a5f10cb3361044bc37e23aea724553
raatamcushwake <raatam.cushwake@gmail.com>
2026-06-17 16:27:37 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://tam-cushman-wakfield.onrender.com",
+        "https://tam-cushman-wakefield.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
